eacc80bb36120a645bedcda39930e7996be88a9a
shievaniR <68860438+shievaniR@users.noreply.github.com>
2020-08-02 16:07:37 +0530

    Update geom.sql

--- sql/geom.sql
+++ sql/geom.sql
@@ -9,4 +9,12 @@ CREATE TABLE btowers(
 ALTER TABLE btowers ADD COLUMN geom geometry(Point, 4326);
 UPDATE btowers SET geom = ST_SetSRID(ST_MakePoint(longitude, latitude), 4326);
 
-SELECT json_build_object('type', 'FeatureCollection','crs',  json_build_object('type','name','properties', json_build_object('name','EPSG:4326')),'features', json_agg(json_build_object('type','Feature','geometry',ST_AsGeoJSON(geom)::json,'properties', json_build_object('LSA',lsa,'District',district,'latitude',latitude,'longitude',longitude,'Sitetype', sitetype)))) FROM btowers;
+SELECT *,ST_Distance(geom,'SRID=4326;POINT(77.30667114257812 28.423448144417215)'::geometry) 
+FROM btowers
+ORDER BY
+btowers.geom <->'SRID=4326;POINT(77.30667114257812 28.423448144417215)'::geometry
+LIMIT 5;
+
+
+SELECT json_build_object('type', 'FeatureCollection','crs',  json_build_object('type','name','properties', json_build_object('name','EPSG:4326')),'features', json_agg(json_build_object('type','Feature','geometry',ST_AsGeoJSON(geom)::json,'properties', json_build_object('LSA',lsa,'District',district,'latitude',latitude,'longitude',longitude,'Sitetype', sitetype)))) FROM ( select * from btowers ORDER BY geom <->'SRID=4326;POINT(77.30667114257812 28.423448144417215)'::geometry LIMIT 5) as InnerQuery
+
